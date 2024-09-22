e0f7cd88c9ee13e3e843a007841a865b38d3b960
Marko <marko.starak.ir.2024@lpnu.ua>
2024-09-22 13:06:10 +0300

    1 commit

---
+++ table1.py
@@ -0,0 +1,23 @@
+from math import log, cos
+
+
+x = 3
+h = 0.2
+a = 3
+b = 8
+
+
+while a <= x <= b:
+    if x < 5:
+        if log(abs(log(x, 3))) == 0:
+            print("x =", x, "y =", "error")
+        else:
+            y = log(abs(log(abs(log(x, 3)), 4)), 5)
+            print("x =", x, "y =", y)
+    elif 5 <= x < 7:
+        y = 1 / (x**2 + 16)
+        print("x =", x, "y =", y)
+    elif x >= 7:
+        y = log(x) + cos(x)
+        print("x =", x, "y =", y)
+    x += h
---
+++ table2.py
@@ -0,0 +1,22 @@
+from math import factorial
+
+
+x = 0
+h = 0.02
+d = 0.001
+a = 0
+b = 0.2
+
+
+while a <= x <= b:
+    element = abs(x**2 - 1)
+    y = 0
+    n = 1
+
+    while element > d:
+        y = y + (x ** (2 * n) - 1) / factorial(2 * n - 1)
+        element = abs((x ** (2 * (n + 1)) - 1) / factorial(2 * (n + 1) - 1))
+        n = n + 1
+
+    print("x =", x, "y =", y)
+    x += h
